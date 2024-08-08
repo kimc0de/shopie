fc7b4b8db94a57d3e3bcf351d34a84fe39560f75
kim <kimngan.ledang@zattoo.com>
2024-08-08 13:51:52 +0200

    Product full schema

--- backend/models/product.js
+++ backend/models/product.js
@@ -2,10 +2,52 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
   name: String,
+  description: {
+    type: string,
+    required: true
+  },
+  richDescription: {
+    type: string,
+    default: ''
+  },
   image: String,
+  images: [{
+    type: String
+  }],
+  brand: {
+    type: string,
+    default: ''
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
   countInStock: {
     type: Number,
-    required: true
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  numReviews: {
+    type: Number,
+    default: 0
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
   }
 });
 
