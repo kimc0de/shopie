ceffdb902ac036dd3ef21da83a49a94eebeed455
kim <kimngan.ledang@zattoo.com>
2024-08-15 13:11:32 +0200

    exclude some routes from auth

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -2,9 +2,18 @@ const {expressjwt} = require('express-jwt');
 
 const authJwt =  () => {
     const secret = process.env.SECRET;
+    const api = process.env.API_URL;
+
     return expressjwt({
         secret,
         algorithms: ['HS256'],
+    }).unless({
+        path: [
+            {url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS']},
+            `/${api}/users/login`,
+            `/${api}/v1/users/register`
+        ]
     })
 };
 
